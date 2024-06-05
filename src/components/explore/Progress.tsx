a3de33fe71eba5ddf09f2bf410c823e38d69b5e0
steven Liao <stevnliao@gmail.com>
2024-06-05 12:02:09 +0800

    chore: ProjectInfo.tsx的版面、Progress大小配置

--- src/components/explore/Progress.tsx
+++ src/components/explore/Progress.tsx
@@ -6,22 +6,22 @@ function Progress({ goal, value }: { goal: number; value: number }) {
 
   return (
     <>
-      <div className="relative my-4 flex flex-nowrap items-center">
+      <div className="relative  flex flex-nowrap items-center">
         <div className="relative flex select-none flex-col items-center justify-center">
-          <svg className="relative flex h-48 w-48 -rotate-90 transform items-center justify-center">
+          <svg className="relative flex h-24 w-24 -rotate-90 transform items-center justify-center">
             <circle
               className="text-slate-400"
               stroke="currentColor"
               strokeWidth="4"
               cx="50%"
               cy="50%"
-              r="48"
+              r="36"
               fill="transparent"
             />
             <circle
               cx="50%"
               cy="50%"
-              r="48"
+              r="36"
               stroke="currentColor"
               id="robin"
               strokeLinecap="round"
@@ -34,7 +34,7 @@ function Progress({ goal, value }: { goal: number; value: number }) {
               className="text-orange-500 transition-all duration-500"
             />
           </svg>
-          <span className="absolute top-20 text-2xl" id="robin1">
+          <span className="absolute top-9 text-lg" id="robin1">
             {percentageValue.toFixed(0) + "%"}
           </span>
         </div>
@@ -44,10 +44,10 @@ function Progress({ goal, value }: { goal: number; value: number }) {
             目標 NT$ {goal.toLocaleString()}
           </span>
 
-          <h2 className="whitespace-nowrap text-2xl font-bold leading-relaxed text-black">
+          <h2 className="whitespace-nowrap text-2xl font-bold leading-relaxed ">
             <span className="sr-only">累計集資金額</span>
           </h2>
-          <h3 className="js-sum-raised text-2xl font-bold text-black">
+          <h3 className="js-sum-raised text-2xl font-bold">
             NT$ {value.toLocaleString()}
           </h3>
 
@@ -57,7 +57,7 @@ function Progress({ goal, value }: { goal: number; value: number }) {
               <h2 className="mr-1 inline-block">
                 <Users />
               </h2>
-              <h3 className="text-zec-green flex items-center text-lg font-bold text-black">
+              <h3 className="text-zec-green flex items-center text-lg font-bold ">
                 <span className="js-backers-count">174</span>人
                 <CircleHelp size={16} color="#2b913c" />
               </h3>
