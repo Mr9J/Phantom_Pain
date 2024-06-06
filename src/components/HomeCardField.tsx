1aabfd757089d3adeada0fee311a55792bce6256
Akashi <a9135787@gmail.com>
2024-06-06 18:01:30 +0800

    6/6 要修一下prjname短的問題

--- src/components/HomeCardField.tsx
+++ src/components/HomeCardField.tsx
@@ -8,30 +8,49 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 import { ProjectCardDTO } from "@/types";
-import { getHomeCardPOP } from "@/services/projects.service";
+import { getHomeCardDayLeft, getHomeCardPOP } from "@/services/projects.service";
 export default function HomeCardField() {
-  const [projects, setProjects] = useState<ProjectCardDTO[] | null>(null);
-
-  useEffect(() => {
-    const fetchProjects = async () => {
-      try {
-        const fetchedProjects = await getHomeCardPOP();
-        setProjects(
-          fetchedProjects.map((project: ProjectCardDTO) => ({
-            ...project,
-          }))
-        );
-        //console.log('fetchedProjects:', fetchedProjects); // 確認資料是否成功加載
-      } catch (error) {
-        console.error("Error fetching projects:", error);
-      }
-    };
-    fetchProjects();
-  }, []);
+    const [POPprojects, setPOPProjects] = useState<ProjectCardDTO[] | null>(null);
+    const [DayLeftprojects, setDayleftProjects] = useState<ProjectCardDTO[] | null>(null);
+    
+    useEffect(() => {
+      const fetchProjects = async () => {
+        try {
+          const fetchedProjects = await getHomeCardPOP();
+          setPOPProjects(
+            fetchedProjects.map((project: ProjectCardDTO) => ({
+              ...project,
+            }))
+          );
+          //console.log('fetchedProjects:', fetchedProjects); // 確認資料是否成功加載
+        } catch (error) {
+          console.error("Error fetching projects:", error);
+        }
+      };
+      fetchProjects();
+    }, []);
+    
+    useEffect(() => {
+      const fetchProjects = async () => {
+        try {
+          const fetchedProjects = await getHomeCardDayLeft();
+          setDayleftProjects(
+            fetchedProjects.map((project: ProjectCardDTO) => ({
+              ...project,
+            }))
+          );
+          //console.log('fetchedProjects:', fetchedProjects); // 確認資料是否成功加載
+        } catch (error) {
+          console.error("Error fetching projects:", error);
+        }
+      };
+      fetchProjects();
+    }, []);
+    
   return (
     <div className="container">
       <div className="flex items-center justify-between p-4 mb-4 lg:px-0">
-        <h2 className="text-2xl font-bold">熱門計畫</h2>
+        <h2 className="text-2xl font-bold">熱門項目</h2>
         <a
           className="inline-block text-base font-bold px-3 py-1 rounded text-primary bg-teal-500 hover:bg-secondary"
           href="/explore"
@@ -41,8 +60,8 @@ export default function HomeCardField() {
       </div>
       <Carousel className="w-full ">
         <CarouselContent className="-ml-1">
-          {projects &&
-            projects.map((item: ProjectCardDTO, index: number) => (
+          {POPprojects &&
+            POPprojects.map((item: ProjectCardDTO, index: number) => (
               <CarouselItem
                 key={index}
                 className="pl-1 md:basis-1/2 lg:basis-1/3"
@@ -61,6 +80,40 @@ export default function HomeCardField() {
         <CarouselPrevious />
         <CarouselNext />
       </Carousel>
+
+      <div className="flex items-center justify-between p-4 mb-4 lg:px-0">
+        <h2 className="text-2xl font-bold">即將到期</h2>
+        <a
+          className="inline-block text-base font-bold px-3 py-1 rounded text-primary bg-teal-500 hover:bg-secondary"
+          href="/explore"
+        >
+          更多 &gt;
+        </a>
+      </div>
+      <Carousel className="w-full ">
+        <CarouselContent className="-ml-1">
+          {DayLeftprojects &&
+            DayLeftprojects.map((item: ProjectCardDTO, index: number) => (
+              <CarouselItem
+                key={index}
+                className="pl-1 md:basis-1/2 lg:basis-1/3"
+              >
+                <div className="p-1">
+                  <ProjectCardVertical prj={item}></ProjectCardVertical>
+                  {/* <Card>
+                  <CardContent className="flex aspect-square items-center justify-center p-6">
+                    <span className="text-2xl font-semibold">{index + 1}</span>
+                  </CardContent>
+                </Card> */}
+                </div>
+              </CarouselItem>
+            ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+
+
     </div>
   );
 }
