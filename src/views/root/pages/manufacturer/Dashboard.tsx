4d4c89c00484d49d90f01fc84c24372aa2de60de
KeYunTinG <zaac2240632@yahoo.com.tw>
2024-06-13 01:23:15 +0800

    Dash

--- src/views/root/pages/manufacturer/Dashboard.tsx
+++ src/views/root/pages/manufacturer/Dashboard.tsx
@@ -4,7 +4,7 @@ import '@/css/style.css';
 
 const Dashboard: React.FC = () => {
   return (
-    <iframe title="V2" width="1600" height="850" src="https://app.powerbi.com/view?r=eyJrIjoiMmFmMGM0MzAtZTYxZS00ZTM0LTkwMzgtMjI0MjhjYjE2M2YwIiwidCI6IjM5OTIzMmZiLTE3ZDEtNDVjYS1iZGE2LTViNTQwNDQxYmQ2MiIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
+    <iframe title="V2" width="1600" height="850" src="https://app.powerbi.com/view?r=eyJrIjoiNDNkNTJmNGYtOGJlYS00YTc4LThjYzUtY2YwZmMxNjE2ZjVjIiwidCI6IjcwODk3ZDZmLTBhNDgtNDlkZS04ODBmLTI3ZDhhZDQ1ZDc2ZSIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
   );
 };
 
