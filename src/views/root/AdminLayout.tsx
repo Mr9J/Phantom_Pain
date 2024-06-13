4c2413d8d576c6af632d69f8705cb34970bcc3dc
Mr9J <91mr.ya@gmail.com>
2024-06-13 11:10:57 +0800

    fix comment

--- src/views/root/AdminLayout.tsx
+++ src/views/root/AdminLayout.tsx
@@ -4,13 +4,16 @@ import AdminSidebar from "@/components/admin/partials/AdminSidebar";
 import Header from "@/components/admin/partials/Header";
 
 const AdminLayout = () => {
-const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-        <>
-              <div className="flex h-screen overflow-hidden">
+    <>
+      <div className="flex h-screen overflow-hidden">
         {/* Sidebar */}
-        <AdminSidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+        <AdminSidebar
+          sidebarOpen={sidebarOpen}
+          setSidebarOpen={setSidebarOpen}
+        />
 
         {/* Content area */}
         <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
@@ -18,9 +21,9 @@ const [sidebarOpen, setSidebarOpen] = useState(false);
           <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
           <Outlet />
           {/* <Banner /> */}
-          </div>
-          </div>
-        </>
+        </div>
+      </div>
+    </>
   );
 };
 
