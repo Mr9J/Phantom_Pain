64384e640cfe45cdf656dd64fc468494faeca883
QUINN LEE <zaac2240632@yahoo.com.tw>
2024-06-14 16:00:28 +0800

    fix bug

--- src/views/root/pages/admin/Projects.tsx
+++ src/views/root/pages/admin/Projects.tsx
@@ -515,7 +515,7 @@ const Projects: React.FC = () => {
                   />
                   <button
                     type="submit"
-                    className="text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm inline-flex items-center text-center"
+                    className="py-2.5 px-5 ms-3 text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm inline-flex items-center text-center"
                   >
                     確認
                   </button>
