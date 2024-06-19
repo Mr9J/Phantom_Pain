8ba550a32f28b3a485ebdd9d11fa64128a1f66ac
QUINN LEE <zaac2240632@yahoo.com.tw>
2024-06-19 11:02:30 +0800

    修改product邏輯

--- src/views/root/pages/manufacturer/Projects.tsx
+++ src/views/root/pages/manufacturer/Projects.tsx
@@ -312,8 +312,9 @@ const Projects = () => {
               <button
                 type="button"
                 onClick={() => {
-                  setvisibleProjectModal(!visibleProjectLg);
-                  setAlter(false);
+                  //setvisibleProjectModal(!visibleProjectLg);
+                  //setAlter(false);
+                  (window.location.href = "/createproject"), "_blank";
                 }}
                 className="mb-2 py-2.5 px-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm  text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
