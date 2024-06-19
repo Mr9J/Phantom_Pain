377a9b406a7a67a8617664f714e52892da70b40f
QUINN LEE <zaac2240632@yahoo.com.tw>
2024-06-19 17:18:33 +0800

    ad

--- src/views/root/pages/manufacturer/Projects.tsx
+++ src/views/root/pages/manufacturer/Projects.tsx
@@ -479,18 +479,22 @@ const Projects = () => {
                             <button
                               type="button"
                               onClick={() => {
-                                setvisibleProjectModal(!visibleProjectLg);
-                                setAlter(true);
-                                setProjectContext([
-                                  item.projectId,
-                                  item.thumbnail,
-                                  item.projectName,
-                                  item.projectDescription,
-                                  item.statusId,
-                                  item.projectGoal,
-                                  item.startDate,
-                                  item.endDate,
-                                ]);
+                                // setvisibleProjectModal(!visibleProjectLg);
+                                // setAlter(true);
+                                // setProjectContext([
+                                //   item.projectId,
+                                //   item.thumbnail,
+                                //   item.projectName,
+                                //   item.projectDescription,
+                                //   item.statusId,
+                                //   item.projectGoal,
+                                //   item.startDate,
+                                //   item.endDate,
+                                // ]);
+                                window.open(
+                                  `/editproject/${item.projectId}`,
+                                  "_blank"
+                                );
                               }}
                               className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 me-2 mb-2"
                             >
