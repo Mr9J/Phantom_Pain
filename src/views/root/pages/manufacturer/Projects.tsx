a204ac01d73e0baef6a1d10da5eede7cf812dce2
QUINN LEE <zaac2240632@yahoo.com.tw>
2024-06-22 13:48:53 +0800

    prettier

--- src/views/root/pages/manufacturer/Projects.tsx
+++ src/views/root/pages/manufacturer/Projects.tsx
@@ -511,14 +511,16 @@ const Projects = () => {
                               className="text-gray-900 bg-white hover:bg-gray-100  focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm  text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 me-2 mb-2"
                               onClick={() => productTableClick(item.projectId)}
                             >
-                               <svg
-        className={`w-[30px] h-[30px] fill-[#262626] dark:fill-[#f2f2f2] transition-transform duration-300 ${
-          productVisible[item.projectId] ? 'rotate-180' : 'rotate-0'
-        }`}
+                              <svg
+                                className={`w-[30px] h-[30px] fill-[#262626] dark:fill-[#f2f2f2] transition-transform duration-300 ${
+                                  productVisible[item.projectId]
+                                    ? "rotate-180"
+                                    : "rotate-0"
+                                }`}
                                 viewBox="0 0 512 512"
                                 xmlns="http://www.w3.org/2000/svg"
                               >
-                                  <path d="M256 464a208 208 0 1 1 0-416 208 208 0 1 1 0 416zM256 0a256 256 0 1 0 0 512A256 256 0 1 0 256 0zM376.9 294.6c4.5-4.2 7.1-10.1 7.1-16.3c0-12.3-10-22.3-22.3-22.3H304V160c0-17.7-14.3-32-32-32l-32 0c-17.7 0-32 14.3-32 32v96H150.3C138 256 128 266 128 278.3c0 6.2 2.6 12.1 7.1 16.3l107.1 99.9c3.8 3.5 8.7 5.5 13.8 5.5s10.1-2 13.8-5.5l107.1-99.9z"></path>
+                                <path d="M256 464a208 208 0 1 1 0-416 208 208 0 1 1 0 416zM256 0a256 256 0 1 0 0 512A256 256 0 1 0 256 0zM376.9 294.6c4.5-4.2 7.1-10.1 7.1-16.3c0-12.3-10-22.3-22.3-22.3H304V160c0-17.7-14.3-32-32-32l-32 0c-17.7 0-32 14.3-32 32v96H150.3C138 256 128 266 128 278.3c0 6.2 2.6 12.1 7.1 16.3l107.1 99.9c3.8 3.5 8.7 5.5 13.8 5.5s10.1-2 13.8-5.5l107.1-99.9z"></path>
                               </svg>
                             </button>
                           </td>
